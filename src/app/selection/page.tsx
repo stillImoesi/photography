9d5081a69c7259d5665b12cb5a67e4291bab8ac3
Peter Imoesi <peter.imoesi@zalando.fi>
2024-07-04 18:13:51 +0300

    fix: update prod endpoint

--- src/app/selection/page.tsx
+++ src/app/selection/page.tsx
@@ -38,7 +38,7 @@ interface ImageUrlResponse {
 const DEV_ENDPOINT =
   "https://rzs6126mm7.execute-api.eu-central-1.amazonaws.com/dev";
 const PROD_ENDPOINT =
-  "https://rzs6126mm7.execute-api.eu-central-1.amazonaws.com";
+  "https://rzs6126mm7.execute-api.eu-central-1.amazonaws.com/prod";
 
   console.log({ test: process.env.NODE_ENV })
 
