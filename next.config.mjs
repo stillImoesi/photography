e05de1c59cc2a9e723e669e9b9b5cb89bb0fea86
Peter Imoesi <peter.imoesi@zalando.fi>
2024-07-04 20:21:28 +0300

    fi: nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: './dist', // Changes the build output directory to `./dist/`.
     pageExtensions: ['ts', 'tsx']
   }
    
