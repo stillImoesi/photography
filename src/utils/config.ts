f8c0b4510126cf5b693e87c4a803f8683cb3ab08
Peter Imoesi <peter.imoesi@zalando.fi>
2024-07-24 12:55:52 +0300

    fix: config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,2 +1,4 @@
 export const ENDPOINT =
-  process.env.NODE_ENV === "production" ? process.env.PROD_ENDPOINT : process.env.DEV_ENDPOINT;
+  (process.env.NODE_ENV === "production"
+    ? process.env.PROD_ENDPOINT
+    : process.env.DEV_ENDPOINT) || "";
