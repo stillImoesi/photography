46d45f98af9af7c7c9b45476902a32214029bf32
Peter Imoesi <peter.imoesi@zalando.fi>
2024-07-26 00:05:20 +0300

    feat: add eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ['ts', 'tsx']
+    pageExtensions: ['ts', 'tsx'],
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
   }
    
   export default nextConfig
--- src/app/create-user/page.tsx
+++ src/app/create-user/page.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { cookies } from "next/headers";
 import CreateUserForm from "./form";
 import RedirectToQuery from "src/components/redirect";
